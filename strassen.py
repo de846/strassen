fc2d9e052c5fbda199f37c4aba596989c5536d79
David Ecker <de846@nyu.edu>
2016-11-13 11:57:23 -0500

    python docstrings should be inside the function

--- strassen.py
+++ strassen.py
@@ -3,13 +3,14 @@ import numpy
 import math
 import pprint as pp 
 
-"""Function to create a random matrix in which values range from 1 to 20"""
 def create_matrix(m):
+    """Function to create a random matrix in which values range from 1 to 20"""
 
     return [[numpy.random.randint(1,20) for a in range(m)] for t in range(m)]
 
-"""Function to split the matrix into 4 equal quarters"""
 def split_matrix(m):
+    """Function to split the matrix into 4 equal quarters"""
+
     row_len = len(m[0][:])
     col_len = len(m[:][0])
     if row_len == col_len and row_len % 2 == 0:
@@ -26,8 +27,9 @@ def split_matrix(m):
     return a,b,c,d
 
 
-"""Addition of two matrices"""
 def matrix_add(a,b):
+    """Addition of two matrices"""
+
     row_len_a = len(a[0][:])
     col_len_a = len(a[:][0])
     row_len_b = len(b[0][:])
@@ -39,8 +41,9 @@ def matrix_add(a,b):
                 c[i][j] = a[i][j] + b[i][j]
     return c
 
-"""Subtraction of two matrices"""
 def matrix_subtract(a,b):
+    """Subtraction of two matrices"""
+
     row_len_a = len(a[0][:])
     col_len_a = len(a[:][0])
     row_len_b = len(b[0][:])
@@ -52,8 +55,9 @@ def matrix_subtract(a,b):
                 c[i][j] = a[i][j] - b[i][j]
     return c
 
-"""Product of two matrices using the regular method."""
 def matrix_product(a,b):
+    """Product of two matrices using the regular method."""
+
     row_len_a = len(a[0][:])
     col_len_a = len(a[:][0])
     row_len_b = len(b[0][:])
@@ -68,6 +72,7 @@ def matrix_product(a,b):
     return c
 
 def matrix_merge(a,b,c,d):
+
     row_len_a = len(a[0][:])
     col_len_a = len(a[:][0])
     row_len_b = len(b[0][:])
@@ -105,7 +110,10 @@ def strassen_solution(a,b):
     row_len_b = len(b[1][:])
     col_len_b = len(b[:][1])
 
-    """Checking condition for whether the rows and columns satisfy the condition to be split into 4 equal quarters. If condition is true, split the matrix and create the 10 sub matrices"""
+    """
+    Checking condition for whether the rows and columns satisfy the condition to be split into 4 equal quarters.
+    If condition is true, split the matrix and create the 10 sub matrices
+    """
     if row_len_a == col_len_a and row_len_b == col_len_b and col_len_a == row_len_b and row_len_a % 2 == 0:
         a11,a12,a21,a22 = split_matrix(a)
         b11,b12,b21,b22 = split_matrix(b)
@@ -120,7 +128,12 @@ def strassen_solution(a,b):
         s9 = matrix_subtract(a11,a21)
         s10 = matrix_add(b11,b12)
 
-        """Implement the recursive condition. The base case is when the original matrix is of size 2. When it reaches the base case, multiplication is done using the regular method to ensure that the product is a 2D array. This ensures easy merging with the merge function"""
+        """
+        Implement the recursive condition. The base case is when the original matrix is of size 2.
+        When it reaches the base case, multiplication is done using the regular method to ensure that
+        the product is a 2D array. This ensures easy merging with the merge function
+        """
+
         if row_len_a > 2:
             p1 = strassen_solution(a11,s1)
         else:
